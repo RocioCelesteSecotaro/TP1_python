98e9fdcb830f89aae8b32c1ecb59b3f34302e714
clari20 <claritamitre@gmail.com>
2024-08-23 12:45:35 -0300

    actualizacion 3

---
+++ TP1/ejercicio1.py
@@ -0,0 +1,2 @@
+nombre = "rocio"
+print("bienvenido "+ nombre)
---
+++ TP1/ejercicio3.py
@@ -0,0 +1,14 @@
+numero1= 25
+numero2= 13
+
+suma= numero1+numero2
+resta= numero1 - numero2
+multiplicacion= numero1*numero2
+division= numero1/numero2
+mod= numero1 % numero2
+
+print("El resultado es: ", suma)
+print("El resultado es: ",resta)
+print("El resultado es: ",multiplicacion)
+print("El resultado es: ",division)
+print("El resultado es: ",mod)
