3f827448aaca881769e7380e60100d7ef10238ee
RocioCelesteSecotaro <rociocelesteflor@gmail.com>
2024-08-24 17:54:22 -0300

    Update ejercicio9.py

--- TP1/ejercicio9.py
+++ TP1/ejercicio9.py
@@ -1,11 +1,9 @@
-numero = 1 
-while numero <= 100:
-   
+print("Numeros divisibles entre 2: ")
+for numero in range(1,101):
     if numero % 2 == 0:
-        print(numero , ' > Divisible por 2')
-    elif numero % 3 == 0:
-       print(numero , ' > Divisible por 3')  
-    else:
-        print(numero) 
-        
-    numero= numero+ 1
+        print(numero)
+
+print("Numeros divisibles entre 3: ")
+for numero in range(1,101):
+    if numero % 3 == 0:
+        print(numero)
