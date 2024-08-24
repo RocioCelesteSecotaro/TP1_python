e902f97dc74f1254f390700a990961ae365625c4
Rocio Celeste Secotaro <rociocelesteflor@gmail.com>
2024-08-24 01:04:42 -0300

    Arreglando ej.6

---
+++ TP1/ejercicio6.py
@@ -0,0 +1,8 @@
+precio=int(input("Ingrese el precio: "))
+sub= precio *1,21
+
+print("Precio ingresado= ",precio)
+print("Precio con IVA= ",sub)
+preciofinal= precio + sub
+
+print("El precio final obtenido fue de: $",preciofinal) #no se porque sale en ()
