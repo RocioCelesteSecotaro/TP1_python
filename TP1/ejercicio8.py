8a5675d4ca74c179792793fccdd9a4996ffbc7bf
RocioCelesteSecotaro <rociocelesteflor@gmail.com>
2024-08-24 17:15:53 -0300

    Add files via upload
    Ejercicios agregados

---
+++ TP1/ejercicio8.py
@@ -0,0 +1,4 @@
+i=1
+for i in range(1,101):
+    print(i)
+
---
+++ TP1/ejercicio9.py
@@ -0,0 +1,11 @@
+numero = 1 
+while numero <= 100:
+   
+    if numero % 2 == 0:
+        print(numero , ' > Divisible por 2')
+    elif numero % 3 == 0:
+       print(numero , ' > Divisible por 3')  
+    else:
+        print(numero) 
+        
+    numero= numero+ 1
