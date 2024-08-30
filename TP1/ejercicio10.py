b14e3d49a436568ac9decf8f11c2a9db497ee348
RocioCelesteSecotaro <rociocelesteflor@gmail.com>
2024-08-30 19:36:57 -0300

    Update ejercicio10.py

--- TP1/ejercicio10.py
+++ TP1/ejercicio10.py
@@ -1,10 +1,10 @@
 while True:
     numero = float(input("Ingresa un numero mayor que 0: "))
-    if numero > 0:
+    if 0<numero :
         print("El numero es correcto!") 
         print("Programa Finalizado") 
         break 
     else:
        print("El número es menor a 0")
        print("ingresalo nuevamente")
-        print(f"El numero ingresado fue {numero}")
+       print(f"El numero ingresado fue {numero}")
