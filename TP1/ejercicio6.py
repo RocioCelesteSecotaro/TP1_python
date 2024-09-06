24c2bc4b5f6cd7db5aa0305317b567bd14ee1369
ElizabethMorato <elizabethmorato19@gmail.com>
2024-09-06 09:07:44 -0300

    Update ejercicio6.py

--- TP1/ejercicio6.py
+++ TP1/ejercicio6.py
@@ -1,3 +1,6 @@
+#Lee un número por teclado que pida el precio de un producto (puede tenerdecimales) 
+# y calcule el precio final con IVA. El IVA sera una constante que sera del 21%. 
+
 precio=float(input("Ingrese el precio: "))
 iva= (precio * 21 )/100
 
