1a9a8f04d9a032e5be3a17b5cd51893eb10796a9
ElizabethMorato <elizabethmorato19@gmail.com>
2024-09-06 09:03:18 -0300

    Update ejercicio1.py

--- TP1/ejercicio1.py
+++ TP1/ejercicio1.py
@@ -1,2 +1,3 @@
+#  Declara una variable que contenga un nombre, después muestra un mensaje de bienvenida por consola con el nombre asignado a la variable
 nombre = "Rocio"
 print("Bienvenid@ "+ nombre+ "!")
