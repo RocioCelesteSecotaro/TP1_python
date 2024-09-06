ea32609a5f20ce5c911f82f5067dc74fb85769e5
ElizabethMorato <elizabethmorato19@gmail.com>
2024-09-06 09:13:13 -0300

    Update ejercicio14.py

--- TP1/ejercicio14.py
+++ TP1/ejercicio14.py
@@ -1,3 +1,29 @@
+#Codifique un programa de consola en Python que permita realizar las siguientes acciones:
+#Generar un número aleatorio entre 0 y 100, para ello use la siguiente función:
+
+#random.randint(0, 100)
+
+#Una vez generado el número codifique la lógica necesaria para encontrar el número
+#aleatorio ayudando al usuario informando al mismo si el número ingresado es mayor o
+#menor al número aleatorio buscado. Una vez encontrado el número muestre la
+#cantidad de intentos necesarios para lograrlo.
+
+#Ejemplo: Número aleatorio generado: 63
+#Ingrese un número entre 0 y 100.
+#Numero Ingresado: 50
+#Respuesta: Es muy bajo
+#Ingrese un número entre 0 y 100.
+#Numero Ingresado: 75
+#Respuesta: Es muy alto
+#Ingrese un número entre 0 y 100.
+#Numero Ingresado: 60
+#Respuesta: Es muy bajo
+#Ingrese un número entre 0 y 100.
+#Numero Ingresado: 65
+#Respuesta: Es muy alto
+#Ingrese un número entre 0 y 100.
+##Numero Ingresado: 63
+#Respuesta: Correcto, numero encontrado, cantidad de intentos 5
 
 import random
 numal= int(random.randint(0, 100))
