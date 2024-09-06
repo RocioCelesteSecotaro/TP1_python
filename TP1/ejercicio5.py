9714a155adff7a044761d82b9e73fa71913ed48b
ElizabethMorato <elizabethmorato19@gmail.com>
2024-09-06 09:07:16 -0300

    Update ejercicio5.py

--- TP1/ejercicio5.py
+++ TP1/ejercicio5.py
@@ -1,3 +1,5 @@
+#Lee un número por teclado e indica si es divisible entre 2 (resto = 0). 
+#Si no lo es,también debemos indicarlo.
 num= int(input ("Ingrese un numero: "))
 if num % 2 == 0:
     print("El numero resulto divisible por 2!")
