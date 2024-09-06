7f9037cb369c24385c656dee499e72a5a564496b
ElizabethMorato <elizabethmorato19@gmail.com>
2024-09-06 09:12:33 -0300

    Update ejercicio13.py

--- TP1/ejercicio13.py
+++ TP1/ejercicio13.py
@@ -1,3 +1,9 @@
+#Pide un número por teclado e indica si es un número primo o no. Un número primo
+#es aquel solo puede dividirse entre 1 y sí mismo. Por ejemplo: 25 no es primo, ya que
+#5 es divisible entre 5, sin embargo, 17 si es primo. Un buen truco para calcular la raíz
+#cuadrada del numero e ir comprobando que si es divisible desde ese número hasta 1.
+#NOTA: Si se introduce un número menor o igual que 1, directamente es no primo. 
+
 numero = int(input("Ingrese un numero: "))
 cont = int(0)
 
