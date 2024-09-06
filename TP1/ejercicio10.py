521b84c3ffa6a63f8d17e4a6875c81f0cd802304
ElizabethMorato <elizabethmorato19@gmail.com>
2024-09-06 09:02:35 -0300

    Update ejercicio10.py

--- TP1/ejercicio10.py
+++ TP1/ejercicio10.py
@@ -1,3 +1,9 @@
+#Crea una aplicación que nos pida un día de la semana y que nos diga si es un dia
+#laboral o no (siendo sábado y domingo no laborales). Usa un switch para ello. Valida
+#que el número ingresado sea un valor entre 1 y 7, caso contrario solicite el valor
+#nuevamente. Debe investigar cuales son las alternativas que se pueden codificar para
+#reemplazar o emular una estructura switch por ejemplo implementando diccionarios.
+#Dado que Python no posee esta estructura por defecto.
 while True:
     numero = float(input("Ingresa un numero mayor que 0: "))
     if 0<numero :
