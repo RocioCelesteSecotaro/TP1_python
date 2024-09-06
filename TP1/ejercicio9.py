4ea861a5b3851f620e0267165e898e64aaae188b
Clari20 <126616185+Clari20@users.noreply.github.com>
2024-09-06 09:35:11 -0300

    Arreglo ejercicio 9

--- TP1/ejercicio9.py
+++ TP1/ejercicio9.py
@@ -2,10 +2,5 @@
 #bucle que desees. 
 print("Numeros divisibles entre 2: ")
 for numero in range(1,101):
-    if numero % 2 == 0:
-        print(numero)
-
-print("Numeros divisibles entre 3: ")
-for numero in range(1,101):
-    if numero % 3 == 0:
+    if numero % 2 == 0 and numero % 3 == 0:
         print(numero)
