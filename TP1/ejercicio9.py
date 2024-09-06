96fad76e3071b304eee58b74ff683124689a9bc0
ElizabethMorato <elizabethmorato19@gmail.com>
2024-09-06 09:09:58 -0300

    Update ejercicio9.py

--- TP1/ejercicio9.py
+++ TP1/ejercicio9.py
@@ -1,3 +1,5 @@
+#Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3. Utiliza el
+#bucle que desees. 
 print("Numeros divisibles entre 2: ")
 for numero in range(1,101):
     if numero % 2 == 0:
