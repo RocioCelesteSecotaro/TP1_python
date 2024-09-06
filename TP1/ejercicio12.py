5545ba582e77e11e3335267be49a23bcba99a380
ElizabethMorato <elizabethmorato19@gmail.com>
2024-09-06 09:11:59 -0300

    Update ejercicio12.py

--- TP1/ejercicio12.py
+++ TP1/ejercicio12.py
@@ -1,4 +1,9 @@
-
+#Crea una aplicación que nos pida un día de la semana y que nos diga si es un dia
+#laboral o no (siendo sábado y domingo no laborales). Usa un switch para ello. Valida
+#que el número ingresado sea un valor entre 1 y 7, caso contrario solicite el valor
+#nuevamente. Debe investigar cuales son las alternativas que se pueden codificar para
+#reemplazar o emular una estructura switch por ejemplo implementando diccionarios.
+#Dado que Python no posee esta estructura por defecto.
 
 dia = int(input("Ingrese un dia de la semana ( 1 - 7 ): "))
 
